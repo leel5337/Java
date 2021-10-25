492a814e5b3265f82ab5f7147fa5cf02baa2722e
wjdgus <5337myung@gmail.com>
2021-10-25 14:18:35 +0900

    변수와 자료형 - 실수

--- src/Chapter1/Java1_01.java
+++ src/Chapter1/Java1_01.java
@@ -2,7 +2,20 @@ package Chapter1;
 
 public class Java1_01 {
     public static void main(String[] args) {
-        String name = "민성";
-        System.out.println(name);
+        char ch = 'A';
+        System.out.println(ch);
+        System.out.println((int)ch);
+
+        int iCh = 66;
+        System.out.println(iCh);
+        System.out.println((char)iCh);
+
+        //char ch2 = -66 (음수값은 x)
+
+        char hangul = '\uAC00';
+        System.out.println(hangul);
+
+        char ch2 = '한';
+        System.out.println(ch2);
     }
 }
