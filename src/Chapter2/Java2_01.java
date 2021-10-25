7e34a617c799438a9cd5976eea75f97280057949
wjdgus <5337myung@gmail.com>
2021-10-25 13:47:39 +0900

    이제 변경x

--- src/Chapter2/Java2_01.java
+++ src/Chapter2/Java2_01.java
@@ -2,7 +2,20 @@ package Chapter2;
 
 public class Java2_01 {
     public static void main(String[] args) {
-        int b = 100;
-        System.out.println(b);
+        char ch = 'A';
+        System.out.println(ch);
+        System.out.println((int)ch);
+
+        int iCh = 66;
+        System.out.println(iCh);
+        System.out.println((char)iCh);
+
+        //char ch2 = -66 (음수값은 x)
+
+        char hangul = '\uAC00';
+        System.out.println(hangul);
+
+        char ch2 = '한';
+        System.out.println(ch2);
     }
 }
