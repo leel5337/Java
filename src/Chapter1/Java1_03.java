ded9bc4a111b80af86b4cd990c9358d5fc0bdc18
wjdgus <5337myung@gmail.com>
2021-10-25 14:19:48 +0900

    변수와 자료형 - 실수

---
+++ src/Chapter1/Java1_03.java
@@ -0,0 +1,32 @@
+package Chapter1;
+
+public class Java1_03 {
+    public static void main(String[] args) {
+
+        /*
+        double dNum = 3.14;
+
+        //float fNum = 3.14; 될려면
+        float fNum = 3.14F;
+
+        System.out.println(dNum);
+        System.out.println(fNum);
+         */
+
+        /*
+        double dNum = 1;
+
+        for ( int i = 0; i <10000; i++) {
+            dNum = dNum + 0.1;
+        }
+        System.out.println(dNum);
+        */
+
+        boolean isMarried = false;
+
+        System.out.println(isMarried);
+
+
+
+    }
+}
