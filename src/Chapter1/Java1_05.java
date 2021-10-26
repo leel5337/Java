f2b63340c237b12c9505eec41178ba1037ef2f5f
wjdgus <5337myung@gmail.com>
2021-10-26 18:03:57 +0900

    증감 연산자

---
+++ src/Chapter1/Java1_05.java
@@ -0,0 +1,18 @@
+package Chapter1;
+
+public class Java1_05 {
+    public static void main(String[] args) {
+        int num1 = -10;
+        int num2 = 20;
+
+        System.out.println(+num1);
+        System.out.println(+num2);
+
+        System.out.println(-num1);
+        System.out.println(-num2);
+
+        num1 = -num1;
+
+        System.out.println(num1);
+    }
+}
