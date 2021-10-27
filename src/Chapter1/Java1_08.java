1be737dc4d9ae45ce794ac4fad56683e76bc4909
wjdgus <5337myung@gmail.com>
2021-10-27 15:20:34 +0900

    if문 2

---
+++ src/Chapter1/Java1_08.java
@@ -0,0 +1,30 @@
+package Chapter1;
+
+import java.util.Scanner;
+
+public class Java1_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score = scanner.nextInt();
+
+        char grade;
+
+        if (score >= 90) {
+            grade = 'A';
+       }
+        else if (score >= 80) {
+            grade = 'B';
+        }
+        else if (score >= 70) {
+            grade = 'C';
+        }
+        else if (score >= 60) {
+            grade = 'D';
+        }
+        else {
+            grade = 'F';
+        }
+        System.out.println("점수 : " + score);
+        System.out.println("학점 : " + grade);
+    }
+}
