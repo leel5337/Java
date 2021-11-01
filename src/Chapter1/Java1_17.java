25ef28ac1f7f280f8285205b67109e7e310514f6
이정현 <5337myung@gmail.com>
2021-11-01 15:46:10 +0900

    break 문

---
+++ src/Chapter1/Java1_17.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+public class Java1_17 {
+    public static void main(String[] args) {
+        int num;
+        for (num = 1; num <= 100; num++) {
+
+            if((num % 3) != 0) continue;
+                System.out.println(num);
+        }
+    }
+}
