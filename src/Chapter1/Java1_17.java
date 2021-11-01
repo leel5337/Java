004fb12b11ce2e37fa300532490563a393131595
이정현 <5337myung@gmail.com>
2021-11-01 15:48:55 +0900

    continue 문

--- src/Chapter1/Java1_17.java
+++ src/Chapter1/Java1_17.java
@@ -2,11 +2,13 @@ package Chapter1;
 
 public class Java1_17 {
     public static void main(String[] args) {
+        
         int num;
         for (num = 1; num <= 100; num++) {
 
             if((num % 3) != 0) continue;
                 System.out.println(num);
+
         }
     }
 }
