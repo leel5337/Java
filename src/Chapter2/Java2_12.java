aa415513fb986be2d5c951e88c206ef9760d41e8
이정현 <5337myung@gmail.com>
2021-11-09 14:46:07 +0900

    this에 대해서 - PersonTest.java

---
+++ src/Chapter2/Java2_12.java
@@ -0,0 +1,13 @@
+//PersonTest.java
+package Chapter2;
+
+public class Java2_12 {
+    public static void main(String[] args) {
+
+        Java2_11 personNoname = new Java2_11();
+        personNoname.showInfo();
+
+        Java2_11 personLee = new Java2_11("Lee", 20);
+        personLee.showInfo();
+    }
+}
