66bf58bc3d002739fc937e55681e021b97baee02
이정현 <5337myung@gmail.com>
2021-11-09 13:42:57 +0900

    정보은닉 DateTest.java

---
+++ src/Chapter2/Java2_10.java
@@ -0,0 +1,15 @@
+package Chapter2;
+
+public class Java2_10 {
+    public static void main(String[] args) {
+
+        Java2_09 date = new Java2_09();
+
+        date.setYear(2021);
+        date.setMonth(11);
+        date.setDay(100);
+
+        date.showDate();
+    }
+
+}
