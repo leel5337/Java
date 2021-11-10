0a4b55e78254d6c4e508781b8e7a6cfc9699379d
이정현 <5337myung@gmail.com>
2021-11-10 16:53:37 +0900

    static 응용 - companyTest.java

---
+++ src/Chapter2/Java2_20.java
@@ -0,0 +1,15 @@
+package Chapter2;
+
+public class Java2_20 {
+
+    public static void main(String[] args) {
+
+        Java2_19 company1 = Java2_19.getInstance();
+
+        Java2_19 company2 = Java2_19.getInstance();
+
+        System.out.println(company1);
+        System.out.println(company2);
+
+    }
+}
